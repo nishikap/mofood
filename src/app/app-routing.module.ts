1abba37102d4decf6380e396ae44a18700244b12
nishikap <nishikapokharel2054@gmail.com>
2021-03-16 19:41:44 -0500

    cart ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { CartComponent } from './cart/cart.component';
+
 
 const routes: Routes = [
   { path:'',component: HomeComponent,pathMatch: 'full'},
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'home',component: HomeComponent }
+  { path: 'home',component: HomeComponent },
+  { path: 'cart',component: CartComponent }
 
 ];
 
